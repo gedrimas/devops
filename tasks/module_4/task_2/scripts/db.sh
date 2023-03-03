3de7cc3c161b3804893e57e80c2a77339d210e26
gedrimas <a.a.gedrimas@gmail.com>
2023-03-03 09:30:50 +0200

    restore commnad was added

---
+++ tasks/module_4/task_2/scripts/db.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+DB="../data/users.db"
+
+validate_user_input() {
+  case $1 in
+    name) while :
+            do
+	      [[ $user =~ [a-zA-Z] ]] && break
+              read -p "Only Latin letters are allowed for user name: " user
+            done;;
+    role) while :
+            do
+	      [[ $role =~ [a-zA-A] ]] && break
+              read -p "Only Latin letters are allowed for user role: " role
+            done;;
+  esac
+}
+
+check_db() {
+  if [ ! -f "$1" ]; then
+      echo "DB does not exist"
+      create_db
+  fi
+}
+
+create_db() {
+  echo "Create users.db file?"
+  select answer in "yes" "no"
+  do
+    case $answer in
+      yes) cd "../data" && touch users.db; break;;
+      no) echo "Good bay"; exit;;
+    esac
+  done
+}
+
+add_user() {
+  check_db $DB
+  read -p "Please enter new user name: " user
+  validate_user_input "name"
+  read -p "Provide the user role: " role
+  validate_user_input "role"
+  echo "_${user}_, _${role}_" >> ../data/users.db
+}
+
+backup_db() {
+  current_date=$(date +%Y-%m-%d)
+  cp $DB "../data/${current_date}-users.db.backup"
+}
+
+restore() {
+  latest_backup=$(find ../data -regex '.*backup' -printf '%T@ %p\n' | sort -n | tail -1 | cut -d" " -f2- )
+  echo "last backup: ${latest_backup}"
+  if [ -f $latest_backup ]; then
+	cat "$latest_backup" > "$DB"
+  else
+	echo "No backup file found"
+  fi
+}
+
+
+case $1 in
+  add) add_user;;
+  help) echo "Help tra-la-la..."; check_db $DB;;
+  backup) backup_db;;
+  restore) restore;;
+  *) check_db $DB;;
+esac
+
