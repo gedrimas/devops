d432157e0bd65dd92a3538d2156df39dc362e937
gedrimas <a.a.gedrimas@gmail.com>
2023-03-09 10:38:52 +0200

    done

--- lab_1/scripts/update-pipline-definition.sh
+++ lab_1/scripts/update-pipline-definition.sh
@@ -5,6 +5,15 @@ if [[ ! -f $1 ]]; then
   exit 1
 fi
 
+which jq > /dev/null
+if [ $? -ne 0 ]; then
+	echo "Error: jq isn't installed."
+	echo "Ubuntu instalation: sudo apt install jq"
+	echo "Mac instalation: brew install jq"
+exit
+fi
+
+
 pipeline_config=$1
 current_date=$(date +%Y-%m-%d)
 updated_pipeline_config="../pipeline-${current_date}.json"
@@ -12,7 +21,7 @@ updated_pipeline_config="../pipeline-${current_date}.json"
 
 branch=main
 owner=""
-pull_from_source_changes=false
+poll_for_source_changes=false
 configuration=""
 
 
@@ -21,37 +30,20 @@ while [[ $# -gt 0 ]]; do
 	case "$1" in
 		--branch) branch="$2"; shift 2;;
 		--owner) owner="$2"; shift 2;;
-		--pull-from-source-changes) pull_from_source_changes=true; shift;; 
+		'--poll-for-source-changes') poll_for_source_changes="$2"; shift 2;; 
 		--configuration) configuration="$2"; shift 2;;
 		*) break;;
 	esac
 done
 
-echo "branch === $branch"
-echo "owner === $owner"
-echo "pull === $pull_from_source_changes"
-echo "configuration === $configuration"
-
-set_configuration() {
-qualityGateEnvVar=$(jq '.pipeline.stages[1].actions[0].configuration.EnvironmentVariables' $pipeline_config)
-buildEnvVar=$(jq '.pipeline.stages[3].actions[0].configuration.EnvironmentVariables' $pipeline_config)
-
-echo $qualityGateEnvVar | sed 's/"value\\":\\.*\\",/\"value\\":\\"REPLACE\\",/' | sed "s/REPLACE/$configuration/"
-
-jq -c '.pipeline.stages[1].actions[0].configuration.EnvironmentVariables | fromjson' $pipeline_config | jq -c '.[]' |\
- jq --arg config "$configuration" '.value = $config'
-
-#echo "TEST $test"
-}
+env_var=$(jq '.pipeline.stages[1].actions[0].configuration.EnvironmentVariables' $pipeline_config)
+updated_env_var=$(echo $env_var | sed 's/"value\\":\\.*\\",/\"value\\":\\"REPLACE\\",/' | sed "s/REPLACE/$configuration/")
 
 jq 'del(.metadata)' $pipeline_config | jq '.pipeline.version = .pipeline.version + 1' |\
  jq --arg owner "$owner" '.pipeline.stages[0].actions[0].configuration.Owner = $owner' |\
  jq --arg branch "$branch" '.pipeline.stages[0].actions[0].configuration.Branch = $branch' |\
- jq --arg pull "$pull_from_source_changes" '.pipeline.stages[0].actions[0].configuration.PollForSourceChanges = $pull'
+ jq --arg pull "$poll_for_source_changes" '.pipeline.stages[0].actions[0].configuration.PollForSourceChanges = $pull' |\
+ jq --argjson env_variable "$updated_env_var" '.pipeline.stages[1].actions[0].configuration.EnvironmentVariables = $env_variable' |\
+ jq --argjson env_variable "$updated_env_var" '.pipeline.stages[3].actions[0].configuration.EnvironmentVariables = $env_variable' > $updated_pipeline_config
 
 
-#'.pipeline.stages[0].actions[0].configuration.Owner = $owner' > $updated_pipeline_config
-#jq '.pipeline.name = 123' "../${updated_pipeline_config}"
-#cat $updated_pipeline_config
-
-set_configuration
